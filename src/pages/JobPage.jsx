ef4698dfc4e247219cfa5d5bf3889b65fa827890
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-07-03 19:18:13 +0530

    impliment data loader to get job by id

---
+++ src/pages/JobPage.jsx
@@ -0,0 +1,51 @@
+// import React from 'react'
+// import { useEffect, useState } from 'react'
+// import { useParams } from 'react-router-dom';
+// import Spinner from '../components/Spinner';
+
+// const JobPage = () => {
+//     const {id} = useParams();
+//     const [job, setJob] = useState(null);
+//     const [loading, setLoading] = useState(true);
+
+//     useState(() => {
+//         const fetchJob = async () => {
+//             try {
+//                 const res = await fetch(`/api/jobs/${id}`);
+//                 const data = await res.json();
+//                 setJob(data);
+
+//             } catch (error) {
+//                 console.log(`Error While Fetching Data: ${error}`);
+//             }finally{
+//                 setLoading(false);
+//             }
+//         }
+
+//         fetchJob();
+
+//     }, []);
+
+//   return loading ? <Spinner /> : <h1>{job.title}</h1>
+
+// }
+
+// export default JobPage
+
+import { useParams, useLoaderData } from "react-router-dom";
+
+
+const JobPage = () => {
+  const { id } = useParams();
+  const job = useLoaderData();
+
+  return <h1>{job.title}</h1>;
+};
+
+const jobLoader = async ({params}) => {
+    const res = await fetch(`/api/jobs/${params.id}`);
+    const data = await res.json();
+    return data;
+};
+
+export {JobPage as default, jobLoader};
